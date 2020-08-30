9c482bf83f8e70366d9053355c17f8bb7560fb03
Diego Gallego <persivalag13@gmail.com>
2020-08-30 18:07:13 -0500

    agrego servidor TCP y recepcion de comandos por comunicacion serial

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
+import 'dart:convert';
 import 'dart:io';
-//import 'package:get_ip/get_ip.dart';
 import 'package:flutter/material.dart';
 import 'dart:typed_data';
 import 'dart:async';
 import 'package:usb_serial/usb_serial.dart';
 import 'package:usb_serial/transaction.dart';
 import 'package:path_provider/path_provider.dart';
-//import 'package:path_provider_ex/path_provider_ex.dart';
 
 void main() => runApp(MyApp());  
 
@@ -23,36 +22,61 @@ class _MyAppState extends State<MyApp> {
   List<Widget> _serialData = [];
   StreamSubscription<String> _subscription;
   Transaction<String> _transaction;
-  int _deviceId;
+  int _deviceId, port = 5000;
   TextEditingController _textController = TextEditingController();
-  //List<StorageInfo> storageInfo = [];
-  String _networkInterface = "";
+  String _networkInterface = "", status = "", route = "", ipAdd = "0.0.0.0";
 
+  sendDataSerial(String line) async{
+    _serialData.add(Text(line));
+        
+    if(line.contains("STATUS")){
+      await _port.write(Uint8List.fromList(status.codeUnits));
+    }
+    else if(line.contains("ROUTE")){
+      await _port.write(Uint8List.fromList(route.codeUnits));
+    }
+
+    if (_serialData.length > 20) {
+      _serialData.removeAt(0);
+    }
+  }
+
+  startTcpServer() async {
+    try{
+      Future<ServerSocket> serverFuture = ServerSocket.bind(ipAdd, port);
+      serverFuture.then((ServerSocket server) {
+        print('Servidor TCP establecido puerto: $port');
+        server.listen((Socket socket) {
+          socket.listen((List<int> data) {
+            String result = String.fromCharCodes(data);
+            if(result.contains("ROUTE")){
+              route = result;
+            } 
+            else if(result.contains("STATUS")){
+              status = result;
+            }
+         });
+       });
+     });
+    }
+    catch(e){
+     print("Error: $e");
+    }
+  }
+  
   getIpInFile() async{
     Directory appDocDir = await getApplicationDocumentsDirectory();
     String doc = appDocDir.path;
-    print("ruta: $doc");
-
-    //storageInfo = await PathProviderEx.getStorageInfo();
-    //print("rutas: $storageInfo");
-
     NetworkInterface.list(includeLoopback: false, type: InternetAddressType.any)
     .then((List<NetworkInterface> interfaces) {
-      setState( () {
-        _networkInterface = "";
-        interfaces.forEach((interface) {
-          _networkInterface += "### name: ${interface.name}\n";
-          int i = 0;
-          interface.addresses.forEach((address) {
-            _networkInterface += "${i++}) ${address.address}\n";
-          });
+      interfaces.forEach((interface) {
+        interface.addresses.forEach((address){
+          _networkInterface = address.address;
+          final File file = File('$doc/ip.txt'); 
+          file.writeAsString(_networkInterface).then((value) async {});
         });
-        print("ip: $_networkInterface");
       });
     });
-
-    //String ipAddress = await GetIp.ipAddress;
-    //print("direccion IP: $ipAddress");
   }
 
   Future<bool> _connectTo(device) async {
@@ -92,21 +116,12 @@ class _MyAppState extends State<MyApp> {
     _deviceId = device.deviceId;
     await _port.setDTR(true);
     await _port.setRTS(true);
-    await _port.setPortParameters(
-        115200, UsbPort.DATABITS_8, UsbPort.STOPBITS_1, UsbPort.PARITY_NONE);
-
-    _transaction = Transaction.stringTerminated(
-        _port.inputStream, Uint8List.fromList([13, 10]));
+    await _port.setPortParameters(9600, UsbPort.DATABITS_8, UsbPort.STOPBITS_1, UsbPort.PARITY_NONE);
+    _transaction = Transaction.stringTerminated(_port.inputStream, Uint8List.fromList([13, 10]));
 
     _subscription = _transaction.stream.listen((String line) { //detecta cuando algo llega por serial
       setState(() {
-        _serialData.add(Text(line));
-        
-        print("acaba de llegar: $line");
-
-        if (_serialData.length > 20) {
-          _serialData.removeAt(0);
-        }
+        sendDataSerial(line);
       });
     });
 
@@ -118,14 +133,13 @@ class _MyAppState extends State<MyApp> {
 
   void _getPorts() async { //obtiene todos los dispositivos conectados
     _ports = [];
+    _puertos = [];
     List<UsbDevice> devices = await UsbSerial.listDevices();
-    print(devices);
-
     devices.forEach((device) {
       String name = device.productName;
       if(name.contains("Mouse") == true || name.contains("mouse") == true || name.contains("MOUSE") == true){
         setState((){
-          print("el dispositivo es un mouse");
+          print("");
         });
       }
       else{
@@ -150,9 +164,10 @@ class _MyAppState extends State<MyApp> {
   void initState() {
     super.initState();
 
+    startTcpServer();
     getIpInFile();
     
-    UsbSerial.usbEventStream.listen((UsbEvent event) {
+    UsbSerial.usbEventStream.listen((UsbEvent event) { //detecta cuando se ha conectado un dispositivo al USB
       _getPorts();
     });
 
@@ -176,37 +191,15 @@ class _MyAppState extends State<MyApp> {
       body: Center(
           child: Column(children: <Widget>[
         Text(
-            //_ports.length > 0
             _puertos.length > 0
                 ? "Dispositivos seriales detectados"
                 : "No hay dispositivos seriales",
             style: Theme.of(context).textTheme.headline6),
         //..._ports,
         Text('Status: $_status\n'),
-        // ListTile(
-        //   title: TextField(
-        //     controller: _textController,
-        //     decoration: InputDecoration(
-        //       border: OutlineInputBorder(),
-        //       labelText: 'Text To Send',
-        //     ),
-        //   ),
-        //   trailing: RaisedButton(
-        //     child: Text("Send"),
-        //     onPressed: _port == null
-        //         ? null
-        //         : () async {
-        //             if (_port == null) {
-        //               return;
-        //             }
-        //             String data = _textController.text + "\r\n";
-        //             await _port.write(Uint8List.fromList(data.codeUnits));
-        //             _textController.text = "";
-        //           },
-        //   ),
-        // ),
         SizedBox(height: 100.0),
         Text("Result Data", style: Theme.of(context).textTheme.headline6),
+        SizedBox(height: 10.0),
         ..._serialData,
       ])),
     ));
